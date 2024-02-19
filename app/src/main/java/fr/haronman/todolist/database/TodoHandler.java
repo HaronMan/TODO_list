3f7642879eec3499c1a5602dabcd965f3bb3e7b6
HaronMan <h.kochbati59@gmail.com>
2024-02-19 15:24:35 +0100

    Implémentation du CRUD

---
+++ app/src/main/java/fr/haronman/todolist/database/TodoHandler.java
@@ -0,0 +1,110 @@
+package fr.haronman.todolist.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.haronman.todolist.model.Todo;
+
+public class TodoHandler extends SQLiteHelper{
+    private SQLiteHelper dbHelper;
+
+    public TodoHandler(Context context){
+        super(context);
+        dbHelper = new SQLiteHelper(context);
+    }
+
+    public int addTodo(Todo todo){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(dbHelper.KEY_TITLE, todo.getTitre());
+        values.put(dbHelper.KEY_DESC, todo.getDescription());
+        values.put(dbHelper.KEY_DATE, todo.getDate());
+        values.put(dbHelper.KEY_DONE, todo.getFait());
+        long insertId = db.insert(dbHelper.TABLE_TODO, null, values);
+        db.close();
+        return (int) insertId;
+    }
+
+    public int updateStudent(Todo todo){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(dbHelper.KEY_TITLE, todo.getTitre());
+        values.put(dbHelper.KEY_DESC, todo.getDescription());
+        values.put(dbHelper.KEY_DATE, todo.getDate());
+        values.put(dbHelper.KEY_DONE, todo.getFait());
+        long updateId=db.update(dbHelper.TABLE_TODO,values,
+                dbHelper.KEY_ID + " = ?",
+                new String[]{String.valueOf(todo.getId())});
+        db.close();
+        return (int) updateId;
+    }
+
+    public Todo getTodo(int id){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Cursor cursor = db.query(dbHelper.TABLE_TODO, new String[] { dbHelper.KEY_ID,
+                        dbHelper.KEY_TITLE, dbHelper.KEY_DESC, dbHelper.KEY_DATE, dbHelper.KEY_DONE }, dbHelper.KEY_ID + "=?",
+                new String[] { String.valueOf(id) }, null, null, dbHelper.KEY_DATE, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+        Todo todo = new Todo(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(2), cursor.getString(3));
+        return todo;
+    }
+
+    public List<Todo> getAllTodo(){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        List<Todo> todoList = new ArrayList<Todo>();
+        String selectQuery = "SELECT * FROM " + dbHelper.TABLE_TODO;
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Todo todo = new Todo(Integer.parseInt(cursor.getString(0)),
+                        cursor.getString(1),cursor.getString(2),cursor.getString(3));
+                todoList.add(todo);
+            } while (cursor.moveToNext());
+        }
+        return todoList;
+    }
+
+    public List<Todo> getAllTodoDone(){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        List<Todo> todoList = new ArrayList<Todo>();
+        String selectQuery = "SELECT * FROM " + dbHelper.TABLE_TODO + "WHERE fait = 1";
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Todo todo = new Todo(Integer.parseInt(cursor.getString(0)),
+                        cursor.getString(1),cursor.getString(2),cursor.getString(3));
+                todoList.add(todo);
+            } while (cursor.moveToNext());
+        }
+        return todoList;
+    }
+
+    public List<Todo> getAllTodoUndone(){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        List<Todo> todoList = new ArrayList<Todo>();
+        String selectQuery = "SELECT * FROM " + dbHelper.TABLE_TODO + "WHERE fait = 0";
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Todo todo = new Todo(Integer.parseInt(cursor.getString(0)),
+                        cursor.getString(1),cursor.getString(2),cursor.getString(3));
+                todoList.add(todo);
+            } while (cursor.moveToNext());
+        }
+        return todoList;
+    }
+
+    public void deleteTodo(Todo todo) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        db.delete(dbHelper.TABLE_TODO, dbHelper.KEY_ID + " = ?",
+                new String[] { String.valueOf(todo.getId()) });
+        db.close();
+    }
+}
--- app/src/main/java/fr/haronman/todolist/model/Todo.java
+++ app/src/main/java/fr/haronman/todolist/model/Todo.java
@@ -4,17 +4,56 @@ import java.util.Date;
 
 public class Todo {
     int id;
-    String titre, description;
-    Date date;
+    String titre, description, date;
     boolean fait;
 
     public Todo(){}
 
-    public Todo(int id, String titre, String description, Date date) {
+    public Todo(int id, String titre, String description, String date) {
         this.id = id;
         this.titre = titre;
         this.description = description;
         this.date = date;
         this.fait = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public boolean getFait(){
+        return fait;
+    }
+
+    public void setFait(boolean fait) {
+        this.fait = fait;
     }
 }
